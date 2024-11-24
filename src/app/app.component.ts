6de4b5731ad3da1405c81a425951e4084c77188f
Jacob Briscoe <41913083+jacob-briscoe@users.noreply.github.com>
2024-11-24 12:07:09 +0000

    refactor: remove Angular Material dependencies and related theming files; update app component and configuration for simplified styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButton],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
